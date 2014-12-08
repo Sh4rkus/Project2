f66132edffdcf36650b26588f57f5557c0f6ca3e
Mark McKinney <m4rkmckinney@gmail.com>
2014-12-07 16:07:46 -0800

    Project 2

---
+++ controller/pokedex.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router  = express.Router();
+var db   = require('../models/db');
+
+router.get('/', function(req, res) {
+    db.getPokemon(function(err, result){
+        if(err) throw err;
+        res.render('pokedex.ejs', {rs: result});
+    });
+});
+
+router.post('/result', function(req, res){
+    console.log(req.body.pokemonName);
+    db.getPokemonDetails(req.body.pokemonName, function(err, result){
+        if(err) throw err;
+        res.render('pokemonDetails.ejs', {rs: result});
+    });
+});
+
+module.exports = router;
